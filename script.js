14aa24bc3bfa6fa654899973e320fc2281203fec
drood <dbernstein1987@hotmail.com>
2018-12-06 19:54:56 +1100

    compare function works now

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ const bands = [
     "Anywhere But Here",
     "An Old Dog"
 ];
+
+function compare(a, b) {
+    const regEx = /^(?:the|a |an )/gim;
+    const aBand = a
+        .toLowerCase()
+        .split(regEx)
+        .filter(Boolean);
+
+    const bBand = b
+        .toLowerCase()
+        .split(regEx)
+        .filter(Boolean);
+
+    const aTrimmed = aBand.toString().trim();
+    const bTrimmed = bBand.toString().trim();
+
+    if (aTrimmed > bTrimmed) {
+        return 1;
+    } else if (aTrimmed < bTrimmed) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+const sortedBands = bands.sort(compare);
+
+console.log(sortedBands);
